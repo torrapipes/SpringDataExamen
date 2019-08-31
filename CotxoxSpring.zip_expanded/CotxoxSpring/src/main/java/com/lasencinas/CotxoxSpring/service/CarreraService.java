4b5b9bceef96941c996ab0e152efb34e31d2a65c
torrapipes <crbergas@gmail.com>
2019-08-31 20:37:08 +0200

    Quinto caso test pasado

--- CotxoxSpring.zip_expanded/CotxoxSpring/src/main/java/com/lasencinas/CotxoxSpring/service/CarreraService.java
+++ CotxoxSpring.zip_expanded/CotxoxSpring/src/main/java/com/lasencinas/CotxoxSpring/service/CarreraService.java
@@ -1,7 +1,29 @@
 package com.lasencinas.CotxoxSpring.service;
 
+import com.lasencinas.CotxoxSpring.domain.Carrera;
+import com.lasencinas.CotxoxSpring.repo.CarreraRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CarreraService {
+
+    @Autowired
+    public CarreraRepo carreraRepo;
+
+    public Long crearCarrera(String tarjeta, String origen, String destino, int distancia, int tiempoEsperado) {
+
+        Carrera carrera = new Carrera(tarjeta);
+        carrera.setOrigen(origen);
+        carrera.setDestino(destino);
+        carrera.setTiempoEsperado(tiempoEsperado);
+        carrera.setDistancia(distancia);
+
+        return carreraRepo.save(carrera).getId();
+
+    }
+
+    public Carrera recuperaCarrera(Long idCarrera) {
+        return carreraRepo.findById(idCarrera).get();
+    }
 }
