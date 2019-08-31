372a683c234790a42e949f9d99fa278f3800a52b
torrapipes <crbergas@gmail.com>
2019-08-31 20:41:57 +0200

    Sexto caso test pasado

---
+++ CotxoxSpring.zip_expanded/CotxoxSpring/src/main/java/com/lasencinas/CotxoxSpring/repo/ConductorRepo.java
@@ -0,0 +1,7 @@
+package com.lasencinas.CotxoxSpring.repo;
+
+import com.lasencinas.CotxoxSpring.domain.Conductor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConductorRepo extends CrudRepository<Conductor, String> {
+}
