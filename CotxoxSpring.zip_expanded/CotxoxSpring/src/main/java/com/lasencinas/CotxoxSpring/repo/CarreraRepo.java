db01feb50293033f48c287189e0a3325da08af29
torrapipes <crbergas@gmail.com>
2019-08-31 20:18:29 +0200

    Segundo y tercer caso test pasado

---
+++ CotxoxSpring.zip_expanded/CotxoxSpring/src/main/java/com/lasencinas/CotxoxSpring/repo/CarreraRepo.java
@@ -0,0 +1,7 @@
+package com.lasencinas.CotxoxSpring.repo;
+
+import com.lasencinas.CotxoxSpring.domain.Carrera;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarreraRepo extends CrudRepository<Carrera, Long> {
+}
