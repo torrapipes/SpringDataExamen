bad40d6aa3a08991cb1f1f96fe053fd9707cff69
torrapipes <crbergas@gmail.com>
2019-09-03 23:31:51 +0200

    folder deleted

--- examen Spring Data.zip_expanded/cotxox/src/main/java/org/mvpigs/cotxox/repo/CarreraRepo.java
+++
@@ -1,8 +0,0 @@
-package org.mvpigs.cotxox.repo;
-import org.mvpigs.cotxox.domain.*;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface CarreraRepo extends CrudRepository<Carrera, Long>{
-
-}
